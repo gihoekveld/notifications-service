6e081945f7762f2f22216e0231a88306530caf39
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-12-13 20:57:46 -0300

    remove sample services

--- src/mail/postmark-mail.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { MailService } from './mail.service';
-
-@Injectable()
-export class PostmarkMaiService implements MailService {
-  sendMail(): string {
-    return 'Postmark Mail!';
-  }
-}
