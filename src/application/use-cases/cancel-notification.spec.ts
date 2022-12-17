443b953689ca53bd027d6a8aef93d700bc7e6053
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-12-17 20:16:36 -0300

    implement factory pattern

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,19 +1,14 @@
-import { Notification } from '@application/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
-import { Content } from '@application/entities/content';
 import { NotificationNotFound } from './errors/notifications-not-found';
+import { makeNotification } from '@test/factories/notification-factory';
 
 describe('Cancel Notification', () => {
   it('should be able to cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
-    const notification = new Notification({
-      category: 'social',
-      content: new Content('Nova solicitação de amizade'),
-      recipientId: 'example-recipient-id',
-    });
+    const notification = makeNotification();
 
     await notificationsRepository.create(notification);
 
