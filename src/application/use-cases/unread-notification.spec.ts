8f621e8946a7f6e7fb5f6360f91426997d86200b
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-12-17 20:40:40 -0300

    create unread notification feature

---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { UnreadNotification } from './unread-notification';
+import { NotificationNotFound } from './errors/notifications-not-found';
+import { makeNotification } from '@test/factories/notification-factory';
+
+describe('Unread Notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('should not be able to unread a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    expect(() => {
+      return unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
